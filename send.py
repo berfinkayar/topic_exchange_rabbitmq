e3985720668eb0701b642abe3782d8712adf6315
berfin.kayar <berfinkaayr@gmail.com>
2023-07-29 15:35:57 +0300

    add topic exchange

--- send.py
+++ send.py
@@ -8,5 +8,7 @@ channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
+
 print(" [x] Sent 'Hello World!'")
 connection.close()
+
